5e30ae6c669f3fe560c70ebcc5be285c49a995ca
numan <numan.codes@gmail.com>
2024-09-09 23:03:46 +0500

    add site map

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css';
-
+<meta name="google-site-verification" content="onII4hxxp8M1XmPi1go7juM5FTCXugQWkFdy7F5i4cU" />
 export const metadata = {
   title: 'Developer Numan - Software Engineer & Web Developer',
   description: 'I am Numan, a passionate software engineer skilled in crafting stellar web experiences using Laravel, Next.js, and React.',
@@ -20,8 +20,12 @@ export const metadata = {
     title: 'Developer Numan - Software Engineer',
     description: 'Skilled in Laravel, Next.js, and React, delivering top-notch web experiences.',
     image: 'https://developernuman.vercel.app/numan.jpeg',
+  },
+  verification: {
+    google: 'onII4hxxp8M1XmPi1go7juM5FTCXugQWkFdy7F5i4cU',
   }
 };
+
 
 export default function Layout({ children }) {
   return (
